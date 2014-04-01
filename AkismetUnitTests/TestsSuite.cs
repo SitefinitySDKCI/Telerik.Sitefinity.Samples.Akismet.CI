4100d97f1016f396e9df01eaef3f7e7193146375
Veronica <veronica.milcheva@telerik.com>
2014-04-01 11:08:07 +0300

    Added Unit Test using JustMock

---
+++ AkismetUnitTests/TestsSuite.cs
@@ -0,0 +1,26 @@
+﻿using MbUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.JustMock;
+
+namespace AkismetUnitTests
+{
+    [TestFixture]
+    public class TestsSuite
+    {
+        [Test]
+        [Author("Very Doge")]
+        [Description("Much testing. WOW")]
+        public static void TestingMockingFramework()
+        {
+            var list = Mock.Create<IList>();
+            var counter = 0;
+            Mock.Arrange(() => list.Add(1)).DoInstead(() => counter++);
+            Assert.AreEqual(list.Count, counter);
+        }
+    }
+}
